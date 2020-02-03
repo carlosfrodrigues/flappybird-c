ce9c97a663288d16b6832607dc799eb0e57bda5d
carlosfrodrigues <cj.felixrodrigues@gmail.com>
2020-02-03 17:43:34 -0300

    fixed the obstacle drawing

--- src/frontend.c
+++ src/frontend.c
@@ -9,30 +9,42 @@ void draw_bird(Bird *bird){
 }
 
 void draw_pipe(Obstacle *obstacle, int ymax){
+  int y1, y2, x1, x2;
+  int y1_down, y2_down, x1_down, x2_down;
   //obstacle->x
   //obstacle->randomCenter = new_random_center(ymax);
 
   //int nRandonNumber = rand()%(my - 8) + 4;
-   int y1 = 0;
-   int y2 = obstacle->randomCenter - 4;
-   int x1 = obstacle->x;
-   int x2 = obstacle->x + 8;
+  y1 = 0;
+  y2 = obstacle->randomCenter - 3;
+  x1 = obstacle->x;
+  x2 = obstacle->x + 8;
 
-   mvhline(y1, x1, 0, x2-x1);
-   mvhline(y2, x1, 0, x2-x1);
-   mvvline(y1, x1, 0, y2-y1);
-   mvvline(y1, x2, 0, y2-y1);
-   mvaddch(y1, x1, ACS_ULCORNER);
-   mvaddch(y2, x1, ACS_LLCORNER);
-   mvaddch(y1, x2, ACS_URCORNER);
-   mvaddch(y2, x2, ACS_LRCORNER);
+  mvhline(y1, x1, 0, x2-x1);
+  mvhline(y2, x1, 0, x2-x1);
+  mvvline(y1, x1, 0, y2-y1);
+  mvvline(y1, x2, 0, y2-y1);
+  mvaddch(y1, x1, ACS_ULCORNER);
+  mvaddch(y2, x1, ACS_LLCORNER);
+  mvaddch(y1, x2, ACS_URCORNER);
+  mvaddch(y2, x2, ACS_LRCORNER);
 
-   /*
-   y1 = nRandonNumber + 2;
-   y2 = my;
-   x1 = 0;
-   x2 = 4;
-   */
+  
+  y1_down = obstacle->randomCenter + 3;
+  y2_down = ymax;
+  x1_down = obstacle->x;
+  x2_down = obstacle->x + 8;
+   
+
+  mvhline(y1_down, x1_down, 0, x2_down - x1_down);
+  mvhline(y2_down, x1_down, 0, x2_down - x1_down);
+  mvvline(y1_down, x1_down, 0, y2_down-y1_down);
+  mvvline(y1_down, x2_down, 0, y2_down - y1_down);
+  mvaddch(y1_down, x1_down, ACS_ULCORNER);
+  mvaddch(y2_down, x1_down, ACS_LLCORNER);
+  mvaddch(y1_down, x2_down, ACS_URCORNER);
+  mvaddch(y2_down, x2_down, ACS_LRCORNER);
+  /* 
    mvhline(obstacle->randomCenter + 4, obstacle->x, 0, 8);
    mvhline(ymax, obstacle->x, 0, 8-obstacle->x);
    mvvline(obstacle->randomCenter + 4, obstacle->x, 0, ymax-obstacle->randomCenter + 4 - obstacle->x);
@@ -41,10 +53,10 @@ void draw_pipe(Obstacle *obstacle, int ymax){
    mvaddch(ymax, 0, ACS_LLCORNER);
    mvaddch(obstacle->randomCenter + 4, 8, ACS_URCORNER);
    mvaddch(ymax, 8, ACS_LRCORNER);
-
-   if(obstacle->next != NULL){
-     draw_pipe(obstacle->next, ymax);
-   }
+*/
+  if(obstacle->next != NULL){
+    draw_pipe(obstacle->next, ymax);
+  }
 }
 
 bool get_keyboard(){
